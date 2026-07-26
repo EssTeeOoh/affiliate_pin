3e4dc27bcb3a5c9e23c2f7ac6e78cb7fb0ff054d
essteeooh <samuelolugbesan@gmail.com>
2026-07-26 15:46:13 +0100

    header update

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,35 +2,38 @@ import { siteConfig } from "@/lib/site";
 
 export default function Head() {
   return (
-    <script
-      type="application/ld+json"
-      dangerouslySetInnerHTML={{
-        __html: JSON.stringify({
-          "@context": "https://schema.org",
-          "@graph": [
-            {
-              "@type": "Organization",
-              name: siteConfig.name,
-              alternateName: siteConfig.tagline,
-              description: siteConfig.description,
-              url: siteConfig.url,
-              logo: `${siteConfig.url}/favicon.ico`
-            },
-            {
-              "@type": "WebSite",
-              name: siteConfig.name,
-              alternateName: siteConfig.tagline,
-              description: siteConfig.description,
-              url: siteConfig.url,
-              potentialAction: {
-                "@type": "SearchAction",
-                target: `${siteConfig.url}/search?q={search_term_string}`,
-                "query-input": "required name=search_term_string"
+    <>
+      <meta name="p:domain_verify" content="59c4ee8c63203cd39876c78cb111c5b3" />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@graph": [
+              {
+                "@type": "Organization",
+                name: siteConfig.name,
+                alternateName: siteConfig.tagline,
+                description: siteConfig.description,
+                url: siteConfig.url,
+                logo: `${siteConfig.url}/favicon.ico`
+              },
+              {
+                "@type": "WebSite",
+                name: siteConfig.name,
+                alternateName: siteConfig.tagline,
+                description: siteConfig.description,
+                url: siteConfig.url,
+                potentialAction: {
+                  "@type": "SearchAction",
+                  target: `${siteConfig.url}/search?q={search_term_string}`,
+                  "query-input": "required name=search_term_string"
+                }
               }
-            }
-          ]
-        })
-      }}
-    />
+            ]
+          })
+        }}
+      />
+    </>
   );
 }
